0baddcaed9d9293c4bdbc357a93cfa78b1d77053
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2018-10-26 18:48:34 +0200

    adding svg logos

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var app = express();
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// serve css
-app.use(express.static('static'));
+// serve css and images
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.render('home');
