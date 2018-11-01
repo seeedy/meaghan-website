a0ac8dd77d47c38df5973041203b2160e8d0b07e
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2018-11-01 16:45:58 +0100

    added contact form

--- public/script.js
+++ public/script.js
@@ -1,22 +1,22 @@
 var hamburger = document.getElementById("hamburger");
 var modal = document.getElementById("modal-outer");
 var x = document.getElementById("nav-x");
-console.log(modal);
+var nav = [].slice.call(document.getElementsByClassName("nav"));
 
-console.log("runinng!!!");
 
 hamburger.addEventListener("click", showModal);
+
+nav.forEach(function(elem) {
+    elem.addEventListener("click", hideModal);
+});
 
 function showModal(e) {
     modal.classList.remove("hidden");
     hamburger.classList.add("hidden");
     x.addEventListener("click", hideModal);
-
 }
 
 function hideModal(e) {
-    console.log("wheee");
     modal.classList.add("hidden");
     hamburger.classList.remove("hidden");
-
 }
