245667e8f87fb7c2f10c9a979aa180ec166ada7c
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2018-11-21 20:30:06 +0100

    vh bug safari

--- public/script.js
+++ public/script.js
@@ -3,6 +3,10 @@ var modal = document.getElementById("modal-outer");
 var x = document.getElementById("nav-x");
 var nav = [].slice.call(document.getElementsByClassName("nav"));
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+var vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 hamburger.addEventListener("click", showModal);
 
